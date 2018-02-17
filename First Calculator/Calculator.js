a52b1fc69aa3426854df8e923182080ac5d96d17
Brittany <BrittanyRussell1993@gmail.com>
2018-02-16 21:25:00 -0500

    Added files via upload

---
+++ First Calculator/Calculator.js
@@ -0,0 +1,77 @@
+console.log("hello world. Brittany was here")
+
+function number1function(){
+
+	document.case.display.value += "1"
+
+};
+function number2function(){
+
+	document.case.display.value += "2"
+};
+function number3function(){
+
+	document.case.display.value += "3"
+};
+function number4function(){
+
+	document.case.display.value += "4"
+};
+function number5function(){
+
+	document.case.display.value += "5"
+};
+
+function number6function(){
+
+	document.case.display.value += "6"
+};
+
+function number7function(){
+
+	document.case.display.value += "7"
+};
+
+function number8function(){
+
+	document.case.display.value += "8"
+};
+
+function number9function(){
+
+	document.case.display.value += "9"
+};
+
+function number0function(){
+
+	document.case.display.value += "0"
+};
+function PlusSign(){
+
+	document.case.display.value += "+"
+};
+function MinusSign(){
+
+	document.case.display.value += "-"
+};
+function DivideSign(){
+
+	document.case.display.value += "/"
+};
+function MultiplySign(){
+
+	document.case.display.value += "*"
+};
+
+function CSign(){
+
+	document.case.display.value = ""
+};
+
+
+function EqualsSign() {
+	var equals = eval(document.case.display.value)
+	document.case.display.value = equals;
+}
+
+
